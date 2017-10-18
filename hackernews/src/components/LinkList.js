55a75bd4a417c46cadfaffd2e7484475b18bd04f
lgants <lgants@gmail.com>
2017-10-18 01:08:01 -0500

    added subscriptions

--- hackernews/src/components/LinkList.js
+++ hackernews/src/components/LinkList.js
@@ -3,6 +3,10 @@ import { graphql, gql } from 'react-apollo'
 import Link from './Link'
 
 class LinkList extends Component {
+  componentDidMount() {
+    this._subscribeToNewLinks()
+    this._subscribeToNewVotes()
+  }
 
   render() {
     if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
@@ -37,6 +41,93 @@ class LinkList extends Component {
     votedLink.votes = createVote.link.votes
     // write data back to the store
     store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
+
+  _subscribeToNewLinks = () => {
+    // document represents the subscription itself; updateQuery determines how the store should be updated with the information that was sent by the server
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Link(filter: {
+            mutation_in: [CREATED]
+          }) {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [
+          subscriptionData.data.Link.node,
+          ...previous.allLinks
+        ]
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
+        }
+        return result
+      }
+    })
+  }
+
+  _subscribeToNewVotes = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Vote(filter: {
+            mutation_in: [CREATED]
+          }) {
+            node {
+              id
+              link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                  id
+                  name
+                }
+                votes {
+                  id
+                  user {
+                    id
+                  }
+                }
+              }
+              user {
+                id
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const votedLinkIndex = previous.allLinks.findIndex(link => link.id === subscriptionData.data.Vote.node.link.id)
+        const link = subscriptionData.data.Vote.node.link
+        const newAllLinks = previous.allLinks.slice()
+        newAllLinks[votedLinkIndex] = link
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
+        }
+        return result
+      }
+    })
   }
 }
 
--- hackernews/src/index.js
+++ hackernews/src/index.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { GC_AUTH_TOKEN } from './constants';
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 import './styles/index.css';
 
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
@@ -11,6 +12,18 @@ import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apol
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj8vsg4dv05g60124i6i9kzjt'
 })
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj8vsg4dv05g60124i6i9kzjt', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 // middleware for authenticating all requests
 networkInterface.use([{
